1e664f0f387be62e8dde9e5a4b21e4614a2bdc3f
Liubov <l.mikhailovna@campus.fct.unl.pt>
2023-11-20 17:33:51 +0000

    Ingredients appear green when selected in pantry.

--- lib/screens/pantry/pantry_screen.dart
+++ lib/screens/pantry/pantry_screen.dart
@@ -14,6 +14,24 @@ class PantryScreen extends StatefulWidget {
 }
 
 class _PantryScreenState extends State<PantryScreen> {
+  List<String> pantryList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    pantryList = widget.pantryList;
+  }
+
+  void updatePantryList(String ingredient) {
+    setState(() {
+      if (pantryList.contains(ingredient)) {
+        pantryList.remove(ingredient);
+      } else {
+        pantryList.add(ingredient);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,22 +58,28 @@ class _PantryScreenState extends State<PantryScreen> {
               itemCount: allIngredients.length,
               itemBuilder: (context, index) {
                 final ingredient = allIngredients[index];
-                final isOwned = widget.pantryList.contains(ingredient);
+                final isOwned = pantryList.contains(ingredient);
 
-                return Container(
-                  decoration: BoxDecoration(
-                    color: isOwned ? Colors.green : Colors.grey,
-                    borderRadius: BorderRadius.circular(20.0), // Adjust the value as needed
-                  ),
-                  width: 100,
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      ingredient,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                return GestureDetector(
+                  onTap: () {
+                    updatePantryList(ingredient);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: isOwned ? Colors.green : Colors.grey,
+                      borderRadius: BorderRadius.circular(
+                          20.0), // Adjust the value as needed
+                    ),
+                    width: 100,
+                    height: 100,
+                    child: Center(
+                      child: Text(
+                        ingredient,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
